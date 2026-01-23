004f6d60e46dfd812522c644e34f5cb1310c17ad
ISAAC WIREDU <0wirei72@solent.ac.uk>
2026-01-23 15:13:40 +0000

    week 1 code

---
+++ week 1 code.py
@@ -0,0 +1,18 @@
+print ("hello world")
+print ("___________\n| o o |\n| ----- |\n-------------  ")
+
+age = 20
+year = 2005
+total = age + year
+
+first_number = 200
+second_number = 300
+
+total = first_number + second_number
+print (total)
+
+price = 0.9
+quantity = 30
+total_price = price * quantity
+
+print (f"You bought {quantity} things for {price}. That costs ${total_price}")
