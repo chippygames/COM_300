7f5d489066db9549884c3155b2386fd938537df2
ISAAC WIREDU <0wirei72@solent.ac.uk>
2026-02-27 15:53:13 +0000

    week 4

---
+++ Week4.py
@@ -0,0 +1,43 @@
+
+#task 1
+# count = 1
+#
+#
+# while count <= 20:
+#     print(count)
+#     count += 1
+#
+
+#task 1.2
+# count = 1
+
+# count = 5
+# while count <= 15:
+#     print(count)
+#     count += 1
+
+
+
+# task 1.3
+#
+
+#
+# count = 1
+# while count <= 21:
+#     print(count)
+#     count += 2
+
+ # task 1.4
+#
+# count = 10
+# while count >= -1:
+#     print(count)
+#     count -= 1
+
+
+# task 2
+
+timestable = int(input("Enter timestable number: "))
+for i in range(0,11):
+   awnser = i * timestable
+   print(f"{i} x {timestable}{awnser}" )
