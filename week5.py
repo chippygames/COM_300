1d41fdfbb05726241635efbc2b3729cc8ee859ef
ISAAC WIREDU <0wirei72@solent.ac.uk>
2026-02-28 20:41:55 +0000

    hell

---
+++ week5.py
@@ -0,0 +1,14 @@
+
+is_student = "yes"
+is_notstudent = "no"
+Age = int(input(" please enter Age: "))
+if Age <= 12: print ("children under 12 get a child ticket ")
+elif Age >= 12: print ('standard ticket available!')
+
+if Age >=12: awnser = input(print("are you a student\nplease enter yes or no:"))
+if awnser == "yes" or awnser == "Yes":
+    print ("you are eligible for a discounted ticket")
+else:
+    print ("you are not eligible for a discounted ticket")
+
+
