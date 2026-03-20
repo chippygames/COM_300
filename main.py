9034c69bea66f8fc3839c935c58ed31cc6033423
ISAAC WIREDU <0wirei72@solent.ac.uk>
2026-03-20 15:45:31 +0000

    week 1 asssesment

---
+++ main.py
@@ -0,0 +1,58 @@
+def assessment_1():
+    order_Value = int(input("please enter your order number"))
+    distance = int(input("please enter your distance"))
+    cost  = 3.95
+
+
+    if distance <= 10:
+        deliveryfee = 0
+    elif distance <= 10:
+        deliveryfee = 10
+    elif distance <= 20:
+        deliveryfee = 15
+    elif distance <= 30:
+        deliveryfee = 20
+
+    total_cost = cost + deliveryfee
+
+    print(f"the total cost is  {total_cost}")
+    print(f"delivery cost is {deliveryfee}")
+    print(f"you are {distance} miles away")
+    print(f"you paid {cost} dollars")
+#simple drone delivery service
+
+def assessment_2():
+    raise NotImplementedError("Assessment 2 has not been attempted")
+
+
+def assessment_3():
+    raise NotImplementedError("Assessment 3 has not been attempted")
+
+
+def assessment_4():
+    raise NotImplementedError("Assessment 4 has not been attempted")
+
+
+def assessment_5():
+    raise NotImplementedError("Assessment 5 has not been attempted")
+
+
+def assessment_6():
+    raise NotImplementedError("Assessment 6 has not been attempted")
+
+
+def assessment_7():
+    raise NotImplementedError("Assessment 7 has not been attempted")
+
+
+if __name__ == "__main__":
+    # Uncomment one of the lines below to test that assessment
+    
+    print(assessment_1())
+    # print(assessment_2())
+    # print(assessment_3())
+    # print(assessment_4())
+    # print(assessment_5())
+    # print(assessment_6())
+    # print(assessment_7())
+    pass
