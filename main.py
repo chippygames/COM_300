078029895774e514dba07175e31b324aedb08c05
ISAAC WIREDU <0wirei72@solent.ac.uk>
2026-03-27 15:33:33 +0000

    assesment 3

--- main.py
+++ main.py
@@ -1,44 +1,80 @@
+from dateutil.utils import within_delta
 
 
-
-def calculate_fee(distance):
-    if distance <= 10:
-        return 0
-    elif distance <= 10:
-        return 10
-    elif distance <= 20:
-        return 15
-    elif distance <= 30:
-        return 15
-    elif distance < 40:
-        return 0
-
-    return
-
-
-def assessment_1():
-    order_Value = int(input("please enter your order number"))
-    distance = int(input("please enter your distance"))
-    cost = 3.95
-
-    deliveryfee = calculate_fee(distance)
-
-
-    total_cost = cost + deliveryfee
-
-    print(f"the total cost is  {total_cost}")
-    print(f"delivery cost is {deliveryfee}")
-    print(f"you are {distance} miles away")
-    print(f"you paid {cost} dollars")
-    return
+#
+# def calculate_fee(distance):
+#     if distance <= 10:
+#         return 0
+#     elif distance <= 10:
+#         return 10
+#     elif distance <= 20:
+#         return 15
+#     elif distance <= 30:
+#         return 15
+#     elif distance < 40:
+#         return 0
+#
+#     return
+#
+#
+# def assessment_1():
+#     order_Value = int(input("please enter your order number"))
+#     distance = int(input("please enter your distance"))
+#     cost = 3.95
+#
+#     deliveryfee = calculate_fee(distance)
+#
+#
+#     total_cost = cost + deliveryfee
+#
+#     print(f"the total cost is  {total_cost}")
+#     print(f"delivery cost is {deliveryfee}")
+#     print(f"you are {distance} miles away")
+#     print(f"you paid {cost} dollars")
+#     return
 #simple drone delivery service
 
 def assessment_2():
-    raise NotImplementedError("Assessment 2 has not been attempted")
+    # num = int(input("please enter a number between 1,20"))
+    # for i in range(1,21):
+    #     if num == i:
+    #         print("x",end="")
+    #     else:
+    #         print("-",end ="")
+    #
+
+
 
 
 def assessment_3():
-    raise NotImplementedError("Assessment 3 has not been attempted")
+    print("please enter the letter which corresponds with your choice\nA-calculate the area of a rectange\nB-calculate the area of a circle\n C-Display a multiplication Table\n D- find the mean of three numbers")
+
+    userinput =  input("")
+    if userinput == "A":
+        Width =  int(input("please enter the width of the rectangle"))
+        Height = int(input("please enter the height of the rectangle"))
+        Area_Rectangle = Width * Height
+        print (f"the area of the rectangle is {Area_Rectangle}")
+    elif userinput == "B":
+       radius = int(input("please enter the radius of the circle"))
+       pi = 3.14159
+       Area_Circle = pi * (radius ** 2)
+       print (f"the area of the circle is {Area_Circle}")
+    elif userinput == "C":
+        Number = int(input("Please enter a number between 1,10"))
+        for i in range(1,11):
+            Times_table = Number * i
+            print (f"the multiplication table for this number is {Times_table}")
+    elif userinput == "D":
+        Num_1 = int(input("please enter a number"))
+        Num_2 = int(input("please enter a number"))
+        Num_3 = int(input("please enter a number"))
+        result = (Num_1 + Num_2 + Num_3) / 3
+        print (f"the mean of the numbers  is {result}")
+   #
+   #
+   #  else:
+
 
 
 def assessment_4():
@@ -60,9 +96,9 @@ def assessment_7():
 if __name__ == "__main__":
     # Uncomment one of the lines below to test that assessment
     
-    print(assessment_1())
+    # print(assessment_1())
     # print(assessment_2())
-    # print(assessment_3())
+    print(assessment_3())
     # print(assessment_4())
     # print(assessment_5())
     # print(assessment_6())
