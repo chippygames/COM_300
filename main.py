674ecaafafcc9fe4c363b45439283f7ecdbd7bed
ISAAC WIREDU <0wirei72@solent.ac.uk>
2026-04-17 18:10:56 +0100

    fix week 4

--- main.py
+++ main.py
@@ -29,16 +29,25 @@
 #     print(f"you are {distance} miles away")
 #     print(f"you paid {cost} dollars")
 #     return
+#finished?
 #simple drone delivery service
 
+
 # def assessment_2():
-    # num = int(input("please enter a number between 1,20"))
-    # for i in range(1,21):
-    #     if num == i:
-    #         print("x",end="")
-    #     else:
-    #         print("-",end ="")
-    #
+#     num = int(input("please enter a number between 1,20"))
+#     if num > 20:
+#         print("sorry the number needs to be less than 20")
+#     else:
+#         for i in range(1,21):
+#             if num == i:
+#                 print("x",end="")
+#             else:
+#                 print("-",end ="")
+#         if num > 20:
+#
+#             print("sorry the number needs to be less than 20")
+# #50-59 screenshot remeber
+# #add comments
 
 
 
@@ -46,43 +55,52 @@
 # def assessment_3():
 #     print("please enter the letter which corresponds with your choice\nA-calculate the area of a rectange\nB-calculate the area of a circle\n C-Display a multiplication Table\n D- find the mean of three numbers")
 #     userinput =  input("")
-#     if userinput == "A":
+#     if userinput == ("A").lower():
 #         Width =  int(input("please enter the width of the rectangle"))
 #         Height = int(input("please enter the height of the rectangle"))
 #         Area_Rectangle = Width * Height
 #         print (f"the area of the rectangle is {Area_Rectangle}")
-#     elif userinput == "B":
+#     elif userinput == ("B").lower():
 #        radius = int(input("please enter the radius of the circle"))
 #        pi = 3.14159
 #        Area_Circle = pi * (radius ** 2)
 #        print (f"the area of the circle is {Area_Circle}")
-#     elif userinput == "C":
+#     elif userinput.lower() == ("C").lower():
 #         Number = int(input("Please enter a number between 1,10"))
 #         for i in range(1,11):
 #             Times_table = Number * i
 #             print (f"the multiplication table for this number is {Times_table}")
-#     elif userinput == "D":
+#     elif userinput == ("D").lower():
 #         Num_1 = int(input("please enter a number"))
 #         Num_2 = int(input("please enter a number"))
 #         Num_3 = int(input("please enter a number"))
 #         result = (Num_1 + Num_2 + Num_3) / 3
 #         print (f"the mean of the numbers  is {result}")
 #     elif userinput not in str(["A","B","C","D"]):
-#         print ("please enter a valid input")
-#
-#     else:
-#        Exit = input("type x to exit Program")
-#         if Exit == "x":
+#         print ("Sorry, your input was not recognised, please enter either A,B,C or D")
+# finished
 
 
-# def assessment_4():
-    #  CoordinateX = int(input("CoordinateX: "))
-    #  CoordinateY = int(input("CoordinateY: "))
-    # for i in range(1,101):
+
+def assessment_4():
+     CoordinateX = int(input("please enter a coordinate between 1 and 10"))
+     CoordinateY = int(input("please enter a coordinate between 1 and 10"))
+     coordinates = CoordinateX * CoordinateY
+     i = 0
+     for row in range (1,11):
+         for col in range(1,11):
+            print("-", end="")
+            if coordinates == row and col:
+                print("X")
+         print()
+
+
+
+# for i in range(1,101):
     #     if i == 10:
     #         print("-,\n")
-    #     for i range 1,11
-    #     print("-",end="")
+    #     for d in range (0,11):
+    #         print("-",end="")
 
 
 # def assessment_5():
@@ -98,7 +116,7 @@
 
 
 
-def assessment_6():
+# def assessment_6():
     #     arr = [6, 5, 3, 1, 2]
     #     arr.sort()
     #     arr[0] = 67
@@ -113,13 +131,18 @@ def assessment_6():
     #      arr[r] = temp_value
 
     # one of these works
-    arr = [6, 5, 3, 1, 2]
-    arr[0] = 67
-    temp_value = arr[2]
-    for r in range(len(arr)):
-        if arr[r] < temp_value:
-            temp_value = arr[r]
-            print(arr[r])
+
+
+    # arr = [6, 5, 3, 1, 2]
+    #
+    # for r in range(len(arr)):
+    #     print (arr)
+    #     arr[0] , arr[r] = arr[r] , arr[0]
+    # print (arr)
+    # print (f"the value stored in arr[0] is {arr[0]}")
+    # this one somehow works
+    #
+
 
 
 
@@ -133,7 +156,7 @@ if __name__ == "__main__":
     # print(assessment_1())
     # print(assessment_2())
     # print(assessment_3())
-    # print(assessment_4())
+    print(assessment_4())
     # print(assessment_5())
-    print(assessment_6())
+    # print(assessment_6())
     # print(assessment_7())
