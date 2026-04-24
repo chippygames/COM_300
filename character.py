eea21893f35acf5ed9d6b4f97b62d8d05245f83b
ISAAC WIREDU <0wirei72@solent.ac.uk>
2026-04-24 16:02:12 +0100

    60-69

---
+++ character.py
@@ -0,0 +1,42 @@
+
+class Character:
+    def __init__(self, name, health, attack,defence):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.defence = defence
+
+    def info(self):
+        return f"Character:{self.name} Health{self.health}/100 | Attack:{self.attack} Defence{self.defence}"
+
+
+    def take_damage(self, damage):
+        self.health = self.health - damage
+        if self.health <= 0:
+            self.health = 0
+            print ("you dead")
+
+
+
+    def heal(self,amount):
+        self.health += amount
+        if self.health >= 100:
+            self.health = 100
+            print ("way too much health")
+
+
+    def attack_target(self,other_character):
+        # if self.Legit_damage >= 0: self.Legit_damage = 0
+        # Character = self.target
+        # self.Legit_damage = damage_
+        #     print f"{self.name} attacks {self.name}"
+
+        damage = self.attack - other_character.defence
+
+        if damage <= 0:
+            damage = 0
+
+        other_character.take_damage(damage)
+        return f"{self.name} attacks {other_character} for {damage} damage!"
+
+
--- main.py
+++ main.py
@@ -1,43 +1,39 @@
 
 
 
-from Charachter import Character
+from character import Character
 
+def assessment_1():
+    order_Value = int(input("please enter your order number"))
+    distance = int(input("please enter your distance"))
+    cost = 3.95
 
-# def calculate_fee(distance):
-#     if distance <= 10:
-#         return 0
-#     elif distance <= 10:
-#         return 10
-#     elif distance <= 20:
-#         return 15
-#     elif distance <= 30:
-#         return 15
-#     elif distance < 40:
-#         return 0
-#
-#     return
-#
-#
-# def assessment_1():
-#     order_Value = int(input("please enter your order number"))
-#     distance = int(input("please enter your distance"))
-#     cost = 3.95
-#
-#     deliveryfee = calculate_fee(distance)
-#
-#
-#     total_cost = cost + deliveryfee
-#
-#     print(f"the total cost is  {total_cost}")
-#     print(f"delivery cost is {deliveryfee}")
-#     print(f"you are {distance} miles away")
-#     print(f"you paid {cost} dollars")
-#     return
-#finished?
-#simple drone delivery service
-import math
-from sys import flags
+    # total_cost = cost + deliveryfee
+    if distance <= 10 and  order_Value >= 100:
+        print ("delivery is free")
+        # return 0
+    elif distance <= 10 and order_Value < 100:
+        print (" delivery is £5")
+        # return 5
+    elif distance < 20:
+        print("delivery is 10")
+        # return 10
+    elif distance <= 30:
+        print("delivery is 15")
+        # return 15
+    else:
+        diffrence = distance - 30
+        extra = distance * 0.5
+        ammount = 15 + extra
+        print (f"your delivery cost{ammount}")
+    #
+    # free_delivery = 10
+    # if  distance  and order_Value <= 100:
+    # print(f"the total cost is {total_cost}")
+    # print(f"delivery cost is {deliveryfee}")
+    # print(f"you are {distance} miles away")
+    # print(f"you paid {cost} dollars")
+    # return
 
 
 # def assessment_2():
@@ -89,101 +85,92 @@ from sys import flags
 
 
 
-def assessment_4():
-     coordinateX = int(input("please enter a coordinate between 1 and 10"))
-     coordinateY = int(input("please enter a coordinate between 1 and 10"))
-
-     for row in range (1,11):
-         for col in range(1,11):
-            if coordinateY == row and coordinateX == col:
-                print("X", end="")
-            else:
-                print("-", end="")
-         print()
-
-
-def assessment_5():
-    # pain fix later "loop everything so it updates after all questions done
-    math_List = []
-    while True:
-        user_Input = input("please enter a choice from the menu\nA-add numbers\nB-Display all values\nC-Replace one number\nD-Calculate the mean\nE-Find the largest number in the list\nX-Exit")
+# def assessment_4():
+#      coordinateX = int(input("please enter a coordinate between 1 and 10"))
+#      coordinateY = int(input("please enter a coordinate between 1 and 10"))
+#
+#      for row in range (1,11):
+#          for col in range(1,11):
+#             if coordinateY == row and coordinateX == col:
+#                 print("X", end="")
+#             else:
+#                 print("-", end="")
+#          print()
 
 
-        if user_Input.lower() == "a":
-            how_many =(int(input("how many numbers you would like to add\n")))
-            for i in range(how_many):
-                number = int(input("please enter your number"))
-                math_List.append(number)
-            print (math_List)
-
-        elif user_Input.lower() == "b":
-            for i in math_List:
-                print(i)
-
-        elif user_Input.lower() == "c":
-            order_new = (int(input("please enter what element you would like to replace")))
-            new_number = (int(input("please enter your number")))
-            print (math_List[order_new])
-            math_List[order_new] = new_number
-            print (math_List)
-
-        elif user_Input.lower() == "d":
-            count = 0
-            tally = 0
-            for i in range(len(math_List)):
-                tally += math_List[i]
-                count += 1
-            awnser = tally/count
-            print(awnser)
-
-        elif user_Input.lower() == "e":
-            big_number = max(math_List)
-            print(f"the largest number in the list is {big_number}")
-
-        elif user_Input.lower() == "x":
-                print("thank you for using this program")
-                break
-
-        else: user_Input.upper() == "A", "B", "C", "D", "E", "X"
-        print("Please enter a valid letter from the menu.")
+# def assessment_5():
+#     # pain fix later "loop everything so it updates after all questions done
+#     math_List = []
+#     while True:
+#         user_Input = input("please enter a choice from the menu\nA-add numbers\nB-Display all values\nC-Replace one number\nD-Calculate the mean\nE-Find the largest number in the list\nX-Exit")
+#
+#
+#         if user_Input.lower() == "a":
+#             how_many =(int(input("how many numbers you would like to add\n")))
+#             for i in range(how_many):
+#                 number = int(input("please enter your number"))
+#                 math_List.append(number)
+#             print (math_List)
+#
+#         elif user_Input.lower() == "b":
+#             for i in math_List:
+#                 print(i)
+#
+#         elif user_Input.lower() == "c":
+#             order_new = (int(input("please enter what element you would like to replace")))
+#             new_number = (int(input("please enter your number")))
+#             print (math_List[order_new])
+#             math_List[order_new] = new_number
+#             print (math_List)
+#
+#         elif user_Input.lower() == "d":
+#             count = 0
+#             tally = 0
+#             for i in range(len(math_List)):
+#                 tally += math_List[i]
+#                 count += 1
+#             awnser = tally/count
+#             print(awnser)
+#
+#         elif user_Input.lower() == "e":
+#             big_number = max(math_List)
+#             print(f"the largest number in the list is {big_number}")
+#
+#         elif user_Input.lower() == "x":
+#                 print("thank you for using this program")
+#                 break
+#
+#         else: user_Input.upper() == "A", "B", "C", "D", "E", "X"
+#         print("Please enter a valid letter from the menu.")
 
 
 
 
 def assessment_6():
-        # arr = [6, 5, 3, 1, 2]
-        # arr.sort()
-        # arr[0] = 67
-        # arr[1] = 69
-        # arr[2] = 71
-        # arr[3] = 72
-        # arr[4] = 73
-        # temp_value = arr[0]
-        # for r in range(len(arr)):
-        #     print(arr[0], arr[r], arr[r + 1])
-        # if arr[r] > temp_value:
-        #  arr[r] = temp_value
 
     arr = [6, 5, 3, 1, 2]
     for r in range(len(arr)):
-        #     print (arr)
-        #     arr[0] , arr[r] = arr[r] , arr[0]
-        # print (arr)
-        print (f"the value stored in arr[0] is {arr[0]} and the other value is {arr[r]}")
+        if arr[r] < arr[0]:
+            temp_value = arr[0]
+            arr[0] = arr[r]
+            arr[r] = temp_value
+    print (arr)
 
 
-
-
-
-
-
- def assessment_7():
+def assessment_7():
 
     player1 = Character("luke",100,50,100)
     player2 = Character("bob",100,50,100)
 
 
-    player1.info()
+    print(player1.info())
+    print(player2.info())
+
+    player1.heal(200)
+
+    player1.attack_target(player2)
+
+    print(player1.info())
 
 
 
@@ -194,10 +181,10 @@ def assessment_6():
 
 if __name__ == "__main__":
 
-    # print(assessment_1())
+    print(assessment_1())
     # print(assessment_2())
     # print(assessment_3())
     # print(assessment_4())
     # print(assessment_5())
     # print(assessment_6())
-    print(assessment_7())
+    # print(assessment_7())
